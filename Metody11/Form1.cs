83fc400f9724f725b0dce7ae02ee5a5db4e8f054
Dajcara.04 <Dajcara.04@A321-09>
2022-03-15 09:30:20 +0100

    Metody11

--- Metody11/Form1.cs
+++ Metody11/Form1.cs
@@ -16,5 +16,36 @@ namespace Metody11
         {
             InitializeComponent();
         }
+
+        public int PocetSlov(string retezec,out string retezecBezCifer)
+        {
+            string s = retezec;
+            char[] oddelovace = { ' ' };
+            retezecBezCifer = s;
+            string[] slova = s.Split(oddelovace, StringSplitOptions.RemoveEmptyEntries);
+            int pocetSlov = slova.Length;
+            retezecBezCifer = s;
+            for (int i = 0; i < retezecBezCifer.Length; ++i)
+            {
+                
+
+                    if (char.IsDigit(retezecBezCifer[i])){
+                    retezecBezCifer = retezecBezCifer.Remove(i, 1);
+                    ++i;
+
+                    }
+                
+            }
+
+            return pocetSlov;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string retezec = textBox1.Text;
+            string s;
+            int pocSlov = PocetSlov(retezec, out s);
+            MessageBox.Show("Pocet slov v retezci je " + pocSlov +" retezec bez cislic je " + s);
+        }
     }
 }

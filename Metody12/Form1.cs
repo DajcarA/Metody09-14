a638d2bd95dcacd5f943b2c7978327973059806e
Aleš Dajčar <dajcara.04@spst.cz>
2022-03-17 17:12:08 +0100

    Metody12

--- Metody12/Form1.cs
+++ Metody12/Form1.cs
@@ -16,5 +16,29 @@ namespace Metody12
         {
             InitializeComponent();
         }
+
+
+        private bool ObsahujeSlovo(string retezec, out string nejdelsiSLovo,out string nejkratsiSlovo)
+        {
+            char[] oddelovace = { ' ' };
+            string[] slova = retezec.Split(oddelovace, StringSplitOptions.RemoveEmptyEntries);
+
+            nejdelsiSLovo = slova.Max();
+            nejkratsiSlovo = slova.Min();
+
+            return (slova.Length > 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string retezec = textBox1.Text;
+            string nejdelSlovo;
+            string nejkratSlovo;
+            if (ObsahujeSlovo(retezec, out nejdelSlovo, out nejkratSlovo))
+            {
+                MessageBox.Show(" Retezec obsahuje alespon jedno slovo, nejdelsi slovo v retezci je " + nejdelSlovo + " a nejkratsi slovo je " + nejkratSlovo);
+            }
+            else MessageBox.Show(" Retezec neobsahuje ani jedno slovo");
+        }
     }
 }
